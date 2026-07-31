9aa5f6228595e972f0b78800318e8049e91002e6
DhruvGupta-2004 <dhruvujjawalgupta@gmail.com>
2026-07-31 16:37:47 +0530

    oh no

--- Projects/Image_processing/Image_processing.srcs/sources_1/new/imageControl.v
+++ Projects/Image_processing/Image_processing.srcs/sources_1/new/imageControl.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 28.07.2026 16:47:27
+// Create Date: 04/01/2020 10:53:27 AM
 // Design Name: 
 // Module Name: imageControl
 // Project Name: 
@@ -19,136 +19,216 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module imageControl(
-    input wire i_clk,
-    input wire i_rst,
-    input wire [7:0] i_pixel_data,
-    input wire i_pixel_data_valid,  // this is coming from camera, saying that now valid data is coming
-    output reg [71:0] o_pixel_data,
-    output wire o_pixel_data_valid
+input                    i_clk,
+input                    i_rst,
+input [7:0]              i_pixel_data,
+input                    i_pixel_data_valid,
+output reg [71:0]        o_pixel_data,
+output                   o_pixel_data_valid,
+output reg               o_intr
+);
 
-    );
-    //these are now the internal wires as we know
-    reg [8:0] pixel_counter;
-    reg [1:0] current_write_line_buffer;
-    reg [3:0] line_buff_data_valid;       //WHY 4 BITS?, there is data_valid in linebuffer.v also....?
-    reg [3:0] line_buff_read_data;
-    reg [1:0] current_read_line_buffer;
-    
-    
-    reg  [11:0] total_pixel_counter;   //why 12 bit, is this for 512 pixels?
-    reg rd_line_buffer;
-    wire [23:0] lb0_data, lb1_data, lb2_data, lb3_data;
-    
-    //FSM states
-    localparam idle = 1'b0;
-    localparam RD_BUFFER = 1'b1;
-    reg read_state;
-    
-    
-    
-    
-    //line buffer instantiation (4)
-linebuffer lb0 (
-.i_clk(i_clk), .i_rst(i_rst), .i_data(i_pixel_data),
-.i_data_valid(line_buff_data_valid[0]),
-.o_data(),  
-.i_rd_data(line_buff_read_data[0]) // ~ ready signal from slave
-    );
-    
-linebuffer lb1 (
-.i_clk(i_clk), .i_rst(i_rst), .i_data(i_pixel_data),
-.i_data_valid(line_buff_data_valid[1]),
-.o_data(),  
-.i_rd_data(line_buff_read_data[1]) // ~ ready signal from slave
-    );
-    
- linebuffer lb2 (
-.i_clk(i_clk), .i_rst(i_rst), .i_data(i_pixel_data),
-.i_data_valid(line_buff_data_valid[2]),
-.o_data(),  
-.i_rd_data(line_buff_read_data[2]) // ~ ready signal from slave
-    );
- 
-linebuffer lb3 (
-.i_clk(i_clk), .i_rst(i_rst), .i_data(i_pixel_data),
-.i_data_valid(line_buff_data_valid[3]),
-.o_data(),  
-.i_rd_data(line_buff_read_data[3]) // ~ ready signal from slave
-    );
-     
-    
-    //code for the demux
-    //count 512 pixels and switch write buffers
-    always @(posedge i_clk) begin
-        if(i_rst) begin
-            pixel_counter <= 9'd0;
-            current_write_line_buffer <= 2'd0;
-        end
-            else if (i_pixel_data_valid) 
+reg [8:0] pixelCounter;
+reg [1:0] currentWrLineBuffer;
+reg [3:0] lineBuffDataValid;
+reg [3:0] lineBuffRdData;
+reg [1:0] currentRdLineBuffer;
+wire [23:0] lb0data;
+wire [23:0] lb1data;
+wire [23:0] lb2data;
+wire [23:0] lb3data;
+reg [8:0] rdCounter;
+reg rd_line_buffer;
+reg [11:0] totalPixelCounter;
+reg rdState;
+
+localparam IDLE = 'b0,
+           RD_BUFFER = 'b1;
+
+assign o_pixel_data_valid = rd_line_buffer;
+
+always @(posedge i_clk)
+begin
+    if(i_rst)
+        totalPixelCounter <= 0;
+    else
+    begin
+        if(i_pixel_data_valid & !rd_line_buffer)
+            totalPixelCounter <= totalPixelCounter + 1;
+        else if(!i_pixel_data_valid & rd_line_buffer)
+            totalPixelCounter <= totalPixelCounter - 1;
+    end
+end
+
+always @(posedge i_clk)
+begin
+    if(i_rst)
+    begin
+        rdState <= IDLE;
+        rd_line_buffer <= 1'b0;
+        o_intr <= 1'b0;
+    end
+    else
+    begin
+        case(rdState)
+            IDLE:begin
+                o_intr <= 1'b0;
+                if(totalPixelCounter >= 1536)
                 begin
-                    pixel_counter <= pixel_counter +1;
-                    if(pixel_counter == 9'd511) begin
-                        current_write_line_buffer <= current_write_line_buffer +1;
-                    end
+                    rd_line_buffer <= 1'b1;
+                    rdState <= RD_BUFFER;
                 end
-    
+            end
+            RD_BUFFER:begin
+                if(rdCounter == 511)
+                begin
+                    rdState <= IDLE;
+                    rd_line_buffer <= 1'b0;
+                    o_intr <= 1'b1;
+                end
+            end
+        endcase
     end
+end
     
-    always @(*) begin
-        line_buff_data_valid = 4'b0000;      //VERY WIERD COMBINATINAL LOGIC, ALWAYS SET TO 0???
-        line_buff_data_valid[current_write_line_buffer] = i_pixel_data_valid;
+always @(posedge i_clk)
+begin
+    if(i_rst)
+        pixelCounter <= 0;
+    else 
+    begin
+        if(i_pixel_data_valid)
+            pixelCounter <= pixelCounter + 1;
     end
-    
-    
-    //pixel tracker
-    //tracks total buffered pixels to decide when to start reading
-    always @(posedge i_clk)begin
-        if (i_rst) begin
-            total_pixel_counter <= 12'd0;
+end
+
+
+always @(posedge i_clk)
+begin
+    if(i_rst)
+        currentWrLineBuffer <= 0;
+    else
+    begin
+        if(pixelCounter == 511 & i_pixel_data_valid)
+            currentWrLineBuffer <= currentWrLineBuffer+1;
+    end
+end
+
+
+always @(*)
+begin
+    lineBuffDataValid = 4'h0;
+    lineBuffDataValid[currentWrLineBuffer] = i_pixel_data_valid;
+end
+
+always @(posedge i_clk)
+begin
+    if(i_rst)
+        rdCounter <= 0;
+    else 
+    begin
+        if(rd_line_buffer)
+            rdCounter <= rdCounter + 1;
+    end
+end
+
+always @(posedge i_clk)
+begin
+    if(i_rst)
+    begin
+        currentRdLineBuffer <= 0;
+    end
+    else
+    begin
+        if(rdCounter == 511 & rd_line_buffer)
+            currentRdLineBuffer <= currentRdLineBuffer + 1;
+    end
+end
+
+
+always @(*)
+begin
+    case(currentRdLineBuffer)
+        0:begin
+            o_pixel_data = {lb2data,lb1data,lb0data};
         end
-        else begin
-            if(i_pixel_data_valid  && !rd_line_buffer)
-                total_pixel_counter <= total_pixel_counter +1;
-            else if (!i_pixel_data_valid && rd_line_buffer)
-                total_pixel_counter <= total_pixel_counter -1;   //  ???why reducing
+        1:begin
+            o_pixel_data = {lb3data,lb2data,lb1data};
         end
-    end
+        2:begin
+            o_pixel_data = {lb0data,lb3data,lb2data};
+        end
+        3:begin
+            o_pixel_data = {lb1data,lb0data,lb3data};
+        end
+    endcase
+end
+
+always @(*)
+begin
+    case(currentRdLineBuffer)
+        0:begin
+            lineBuffRdData[0] = rd_line_buffer;
+            lineBuffRdData[1] = rd_line_buffer;
+            lineBuffRdData[2] = rd_line_buffer;
+            lineBuffRdData[3] = 1'b0;
+        end
+       1:begin
+            lineBuffRdData[0] = 1'b0;
+            lineBuffRdData[1] = rd_line_buffer;
+            lineBuffRdData[2] = rd_line_buffer;
+            lineBuffRdData[3] = rd_line_buffer;
+        end
+       2:begin
+             lineBuffRdData[0] = rd_line_buffer;
+             lineBuffRdData[1] = 1'b0;
+             lineBuffRdData[2] = rd_line_buffer;
+             lineBuffRdData[3] = rd_line_buffer;
+       end  
+      3:begin
+             lineBuffRdData[0] = rd_line_buffer;
+             lineBuffRdData[1] = rd_line_buffer;
+             lineBuffRdData[2] = 1'b0;
+             lineBuffRdData[3] = rd_line_buffer;
+       end        
+    endcase
+end
     
-    //read fsm and read counters
-    always @(posedge i_clk) begin
-        if(i_rst) begin
-            read_state <= IDLE;
-            rd_line_buffer <= 1'b0;
-            current_read_line_buffer <= 2'd0;
-        end else begin
-            case (read_state)
-                IDLE: begin
-                    //wait until at least 3 lines are full
-                    if (total_pixel_counter >= 12d'1536) begin
-                        read_state <= RD_BUFFER;    //what is this RD BUFFER
-                        rd_line_buffer <= 1'b1;
-                        
-                     end
-                  end 
-                  RD_BUFFER: begin
-                    if(read_conter == 9'd511) begin
-                        read_
-                    end
-                    
-                  
-                  end
-                        
-                
-                
-                
-    
-    end 
-    
-    
-    
-    
-    
+lineBuffer lB0(
+    .i_clk(i_clk),
+    .i_rst(i_rst),
+    .i_data(i_pixel_data),
+    .i_data_valid(lineBuffDataValid[0]),
+    .o_data(lb0data),
+    .i_rd_data(lineBuffRdData[0])
+ ); 
+ 
+ lineBuffer lB1(
+     .i_clk(i_clk),
+     .i_rst(i_rst),
+     .i_data(i_pixel_data),
+     .i_data_valid(lineBuffDataValid[1]),
+     .o_data(lb1data),
+     .i_rd_data(lineBuffRdData[1])
+  ); 
+  
+  lineBuffer lB2(
+      .i_clk(i_clk),
+      .i_rst(i_rst),
+      .i_data(i_pixel_data),
+      .i_data_valid(lineBuffDataValid[2]),
+      .o_data(lb2data),
+      .i_rd_data(lineBuffRdData[2])
+   ); 
+   
+   lineBuffer lB3(
+       .i_clk(i_clk),
+       .i_rst(i_rst),
+       .i_data(i_pixel_data),
+       .i_data_valid(lineBuffDataValid[3]),
+       .o_data(lb3data),
+       .i_rd_data(lineBuffRdData[3])
+    );    
     
 endmodule
